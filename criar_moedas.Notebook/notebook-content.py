ba4f8345d74d0c221fce6bffa547d6c9b3b58e77
Eduardo Padilha <36409740+Padilhovisck@users.noreply.github.com>
2026-02-20 17:57:18 -0300

    Criados Lakes

---
+++ criar_moedas.Notebook/notebook-content.py
@@ -0,0 +1,31 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "21dc259f-a161-44c2-9437-8d3deaeb906d",
+# META       "default_lakehouse_name": "lake",
+# META       "default_lakehouse_workspace_id": "3eb1a572-23f9-4615-9dd9-9c5a03e95de2",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "21dc259f-a161-44c2-9437-8d3deaeb906d"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
